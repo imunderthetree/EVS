1f7390f19eb154ad59137ca1eee9f1060b6b294a
Yusuf Alsaied <xenonae2@gmail.com>
2025-12-06 13:52:41 +0200

    start

---
+++ Project/EVS/Pages/Shared/_ParentSidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EVS.Pages.Shared
+{
+    public class _ParentSidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
