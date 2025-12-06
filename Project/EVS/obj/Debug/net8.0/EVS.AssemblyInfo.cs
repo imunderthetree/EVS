1d5557fc37fac70317bda3efb877c30dff91740c
Yusuf Alsaied <xenonae2@gmail.com>
2025-12-06 13:54:32 +0200

    still working on the skeleton files

--- Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
+++ Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5004c356e15036c701284d90636c01d1541987")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7390f19eb154ad59137ca1eee9f1060b6b294a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
