2d9d48fbdf3c956a61030a8ee5393ec407490c78
Ali ahmed <s-ali.ibrahim@zewailcity.edu.eg>
2025-12-12 18:29:34 +0200

    .

--- Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
+++ Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f147a4c54527f9aaab2e03d484626db2b30edc29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb046edb47c085c7c8ce3deb633276e34646e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
