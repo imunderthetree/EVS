9a7e1d5b92a7ab9cb8373b8fd86890021e6e0758
Yusuf Alsaied <xenonae2@gmail.com>
2025-12-12 17:11:58 +0200

    finished student dashboard

--- Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
+++ Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e191c368006766006a829dd607ba227fbb0d7f75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf2f49c905ec327ecdce2d1a8c00e330fd8d316")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
