6c4677fbbdfc74278849fba0e8a4ee089c3c90a8
Yusuf Alsaied <xenonae2@gmail.com>
2025-12-13 09:46:45 +0200

    ok

--- Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
+++ Project/EVS/obj/Debug/net8.0/EVS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EVS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb046edb47c085c7c8ce3deb633276e34646e13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2cb6f0d18e459a4ad3d37a2031337ae4f93f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("EVS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EVS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
